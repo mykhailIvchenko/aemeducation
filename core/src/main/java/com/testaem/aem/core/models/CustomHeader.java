91f2b2adf5e92ee5d8580f829b267719af81b2c6
mykhailIvchenko <mivchenko@exadel.com>
2022-06-10 20:02:19 +0300

    Custom footer/header/page (single news) has been added to project.

---
+++ core/src/main/java/com/testaem/aem/core/models/CustomHeader.java
@@ -0,0 +1,6 @@
+package com.testaem.aem.core.models;
+
+public interface CustomHeader {
+    String getImagePath();
+    String getText();
+}
---
+++ core/src/main/java/com/testaem/aem/core/models/NewsPage.java
@@ -0,0 +1,10 @@
+package com.testaem.aem.core.models;
+
+import java.util.Date;
+
+public interface NewsPage {
+    String getTextHTML();
+    String getText();
+    String getImagePath();
+    String getDate();
+}
